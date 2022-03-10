722ecf8d79c9019d8e250a82f24dc7de09a5d178
Bianca Popa <biancapopa2002@gmail.com>
2022-03-10 23:16:35 +0200

    lab1: Add solutions to lab
    Signed off-by: Bianca Popa <biancapopa2002@gmail.com>

--- laborator/content/reprezentare-numere/4-rotations/rotations.c
+++ laborator/content/reprezentare-numere/4-rotations/rotations.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
+#define SIZE 8
+#define LEFT 1
+#define RIGHT 256
 
 void rotate_left(int *number, int bits)
 {
-	/* TODO */
-	(void) number;
-	(void) bits;
+	for (int i = 0; i < bits; ++i) {
+		int temp = (*number >> SIZE) & LEFT;
+	*number = (*number << 1) | temp;
+	}
 }
 
 void rotate_right(int *number, int bits)
 {
-	/* TODO */
-	(void) number;
-	(void) bits;
+	for (int i = 0; i < bits; ++i) {
+		int temp = (*number << SIZE) & RIGHT;
+		*number = (*number >> 1) | temp;
+	}
 }
 
 int main()
 {
-	/* TODO: Test functions */
+	int number, bits;
+	scanf("%d%d", &number, &bits);
 
+	bits %= SIZE;
+	rotate_left(&number, bits);
+	rotate_right(&number, bits);
+
+	printf("%d\n", number);
 	return 0;
 }
-
