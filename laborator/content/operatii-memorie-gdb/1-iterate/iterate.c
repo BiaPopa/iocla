5c200c743e10b84bba92d06202b236cdbf2fc00f
Bianca Popa <biancapopa2002@gmail.com>
2022-03-16 21:47:23 +0200

    Lab2: ex1: Add solution for exercise 1`
    Signed-off-by: Bianca Popa <biancapopa2002@gmail.com>

--- laborator/content/operatii-memorie-gdb/1-iterate/iterate.c
+++ laborator/content/operatii-memorie-gdb/1-iterate/iterate.c
@@ -10,7 +10,31 @@
 int main() {
     int v[] = {0xCAFEBABE, 0xDEADBEEF, 0x0B00B135, 0xBAADF00D, 0xDEADC0DE};
 
-    (void) v;
+    unsigned char *char_ptr = (unsigned char *)v;
+
+    int i = 0;
+    while (i < 20) {
+        printf("%p -> 0x%x\n", char_ptr + i, *(char_ptr + i));
+        ++i;
+    }
+
+    printf("\n");
+
+    unsigned short *ptr = (unsigned short *)v;
+    i = 0;
+    while (i < 10) {
+        printf("%p -> 0x%x\n", ptr + i, *(ptr + i));
+        ++i;
+    }
+
+    printf("\n");
+
+    unsigned int *ptr2 = (unsigned int *)v;
+    i = 0;
+    while (i < 5) {
+        printf("%p -> 0x%x\n", ptr2 + i, *(ptr2 + i));
+        ++i;
+    }
 
     return 0;
 }
