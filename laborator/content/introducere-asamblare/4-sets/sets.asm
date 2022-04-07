a2c3d54bcac5baa596a93e3d68ec716cc104011e
Bianca Popa <biancapopa2002@gmail.com>
2022-04-07 21:47:42 +0300

    Lab5: ex4: Add solution for exercise 4.
    Signed-off-by: Bianca Popa <biancapopa2002@gmail.com>

--- laborator/content/introducere-asamblare/4-sets/sets.asm
+++ laborator/content/introducere-asamblare/4-sets/sets.asm
@@ -12,22 +12,30 @@ main:
     PRINTF32 `%u\n\x0`, ebx ; afiseaza cea de-a doua multime
 
     ; TODO1: reuniunea a doua multimi
-
+    or eax, ebx
+    PRINTF32 `%u\n\x0`, eax ; afiseaza prima multime
 
     ; TODO2: adaugarea unui element in multime
-
+    or eax, 64
+    PRINTF32 `%u\n\x0`, eax ; afiseaza prima multime
 
     ; TODO3: intersectia a doua multimi
-
+    and eax, ebx
+    PRINTF32 `%u\n\x0`, eax ; afiseaza prima multime
 
     ; TODO4: complementul unei multimi
-
+    not eax
+    PRINTF32 `%u\n\x0`, eax ; afiseaza prima multime
 
     ; TODO5: eliminarea unui element
-
+    or eax, 1
+    not eax
+    PRINTF32 `%u\n\x0`, eax ; afiseaza prima multime
 
     ; TODO6: diferenta de multimi EAX-EBX
-
+    not ebx
+    and eax, ebx
+    PRINTF32 `%u\n\x0`, eax ; afiseaza prima multime
 
     xor eax, eax
     ret
